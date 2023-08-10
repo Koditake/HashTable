e79bd9dc56333771a8cb771970948808c41f2837
Koditake <thientuannguyen95@gmail.com>
2023-08-10 15:50:50 +0700

    Update gitignore and delete table function

--- src/hash_table.h
+++ src/hash_table.h
@@ -11,3 +11,12 @@ typedef struct
     int size;
     ht_item** items;
 } ht_hash_table;
+
+
+/*
+    Prototypes
+*/
+
+ht_item* ht_new_item(const char* key, const char* value);
+ht_hash_table* ht_new();
+void ht_del_hash_table(ht_hash_table*);
